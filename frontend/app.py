5c8eb925dbd56ac1b0f044b2b1f66d34d24bf7ba
Arghyadeep Deb <arghyadeepdeb020703@gmail.com>
2026-03-25 15:05:00 +0530

    Update frontend backend URL to Render deployment

--- frontend/app.py
+++ frontend/app.py
@@ -8,7 +8,7 @@ import requests
 import streamlit as st
 
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
+BACKEND_URL = os.getenv("BACKEND_URL", "https://credit-scoring-system-spt2.onrender.com")
 
 
 @st.cache_data(show_spinner=False)
